fd804f490ce77b19418af0e6c1e72d5940b9431d
slborisova <141890054+slborisova@users.noreply.github.com>
2024-10-18 18:09:34 -0400

    Locators lesson (#7)
    * my commit
    
    * firstTest fils added
    
    * locators methods added

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,6 +3,56 @@ import {test} from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/');
+
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click();
+});
+
+test('Locator syntax rules', async ({page}) => {
+    // by Tag name
+    await page.locator('input').first().click();
+
+    // by ID
+    page.locator('#inputEmail1');
+
+    // by Class value
+    page.locator('.shape-rectangle');
+
+    // by Attribute
+    page.locator('[placeholder="Email"]');
+
+    // by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+
+    // Combine different selectors
+    page.locator('input[placeholder="Email"][nbinput]');
+
+    // By XPath (not recommended)
+    page.locator('//*[@id="inputEmail1"]');
+
+    // By partial text match
+    page.locator(':text("Using")');
+
+    // By exact text math
+    page.locator(':text-is("Using the Grid")')
+})
+
+test.only('User facing locators', async({page}) => {
+    await page.getByRole('textbox', {name: "Email"}).first().click(); // By Role
+    await page.getByRole('button', {name: "Sign in"}).first().click();
+
+    await page.getByLabel('Email').first().click(); // By Label
+
+    await page.getByPlaceholder('Jane Doe').click(); //By Placeholder
+
+    await page.getByText('Using the Grid').click(); //By Text
+
+    await page.getByTestId('SignIn').click(); // By data test id
+
+    await page.getByTitle('IoT Dashboard').click(); // By Title
+
+})
+
 });
 
 test.describe.only('suite1', () => {
@@ -32,4 +82,5 @@ test.describe('suite1', () => {
         await page.getByText('Datepicker').click();
     });
 })
+
 
