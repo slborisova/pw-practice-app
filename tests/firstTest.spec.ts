833a769cb23ba34b1160016d1ba998352daf96f7
slborisova <slborisova@gmail.com>
2024-10-18 22:16:06 -0400

    more locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,86 +1,79 @@
-import {test} from '@playwright/test';
+import { test } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
 
-test.beforeEach(async ({page}) => {
-    await page.goto('http://localhost:4200/');
-
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click();
+  //   await page.getByText("Forms").click();
+  //   await page.getByText("Form Layouts").click();
 });
 
-test('Locator syntax rules', async ({page}) => {
-    // by Tag name
-    await page.locator('input').first().click();
+test("Locator syntax rules", async ({ page }) => {
+  // by Tag name
+  await page.locator("input").first().click();
 
-    // by ID
-    page.locator('#inputEmail1');
+  // by ID
+  page.locator("#inputEmail1");
 
-    // by Class value
-    page.locator('.shape-rectangle');
+  // by Class value
+  page.locator(".shape-rectangle");
 
-    // by Attribute
-    page.locator('[placeholder="Email"]');
+  // by Attribute
+  page.locator('[placeholder="Email"]');
 
-    // by Class value (full)
-    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+  // by Class value (full)
+  page.locator(
+    '[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]'
+  );
 
-    // Combine different selectors
-    page.locator('input[placeholder="Email"][nbinput]');
+  // Combine different selectors
+  page.locator('input[placeholder="Email"][nbinput]');
 
-    // By XPath (not recommended)
-    page.locator('//*[@id="inputEmail1"]');
+  // By XPath (not recommended)
+  page.locator('//*[@id="inputEmail1"]');
 
-    // By partial text match
-    page.locator(':text("Using")');
+  // By partial text match
+  page.locator(':text("Using")');
 
-    // By exact text math
-    page.locator(':text-is("Using the Grid")')
-})
-
-test.only('User facing locators', async({page}) => {
-    await page.getByRole('textbox', {name: "Email"}).first().click(); // By Role
-    await page.getByRole('button', {name: "Sign in"}).first().click();
-
-    await page.getByLabel('Email').first().click(); // By Label
-
-    await page.getByPlaceholder('Jane Doe').click(); //By Placeholder
-
-    await page.getByText('Using the Grid').click(); //By Text
-
-    await page.getByTestId('SignIn').click(); // By data test id
-
-    await page.getByTitle('IoT Dashboard').click(); // By Title
-
-})
-
+  // By exact text math
+  page.locator(':text-is("Using the Grid")');
 });
 
-test.describe.only('suite1', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Charts').click()
-    });
+test("User facing locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click(); // By Role
+  await page.getByRole("button", { name: "Sign in" }).first().click();
 
-    test('the first test', async ({page}) => {
-        await page.getByText('Form Layouts').click()
-    });
-    
-    test('navigate to datepicker page', async ({page}) => {
-        await page.getByText('Datepicker').click()
-    });
+  await page.getByLabel("Email").first().click(); // By Label
+
+  await page.getByPlaceholder("Jane Doe").click(); //By Placeholder
+
+  await page.getByText("Using the Grid").click(); //By Text
+
+  await page.getByTestId("SignIn").click(); // By data test id
+
+  await page.getByTitle("IoT Dashboard").click(); // By Title
 });
 
-test.describe('suite1', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Forms').click();
-    });
+test.describe("Chart tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts").first().click();
+    await page.getByText("Echarts").click();
+  });
 
-    test('the first test1', async ({page}) => {
-        await page.getByText('Form Layouts').click();
-    });
-    
-    test('navigate to datepicker page1', async ({page}) => {
-        await page.getByText('Datepicker').click();
-    });
-})
+  test("the Pie test", async ({ page }) => {
+    await page.getByText("Pie").isVisible();
+  });
+});
 
+test.describe("Form tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
 
+  test("the first test1", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("navigate to datepicker page1", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
+});
