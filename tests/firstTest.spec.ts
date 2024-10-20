881524230eadb52c2895d9b0ad96f9350e3ed3d4
slborisova <slborisova@gmail.com>
2024-10-20 14:23:15 -0400

    new locators added

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -3,8 +3,8 @@ import { test } from "@playwright/test";
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
 
-  //   await page.getByText("Forms").click();
-  //   await page.getByText("Form Layouts").click();
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
 });
 
 test("Locator syntax rules", async ({ page }) => {
@@ -77,3 +77,12 @@ test.describe("Form tests", () => {
     await page.getByText("Datepicker").click();
   });
 });
+
+test.only('Locating child elements', async ({page}) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click();// same as next line locators
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();//chaining
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click();// try to avoid to select by index
+})
