c67f419fda41893f2ee7857ccfb658b4fb8ac4b7
slborisova <141890054+slborisova@users.noreply.github.com>
2024-10-20 22:57:07 -0400

    Locators lesson (parent elements) (#8)
    * my commit
    
    * firstTest fils added
    
    * locators methods added
    
    * more locators
    
    * new locators added
    
    * locating parent elements

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,109 +1,108 @@
-import { NbMenuItem } from '@nebular/theme';
+import { NbMenuItem } from "@nebular/theme";
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  
   {
-    title: 'IoT Dashboard',
-    icon: 'home-outline',
-    link: '/pages/iot-dashboard',
+    title: "IoT Dashboard",
+    icon: "home-outline",
+    link: "/pages/iot-dashboard",
   },
   {
-    title: 'FEATURES',
+    title: "FEATURES",
     group: true,
   },
   {
-    title: 'Forms',
-    icon: 'edit-2-outline',
+    title: "Forms",
+    icon: "edit-2-outline",
     children: [
       {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
+        title: "Form Layouts",
+        link: "/pages/forms/layouts",
       },
       {
-        title: 'Datepicker',
-        link: '/pages/forms/datepicker',
+        title: "Datepicker",
+        link: "/pages/forms/datepicker",
       },
     ],
   },
   {
-    title: 'Modal & Overlays',
-    icon: 'browser-outline',
+    title: "Modal & Overlays",
+    icon: "browser-outline",
     children: [
       {
-        title: 'Dialog',
-        link: '/pages/modal-overlays/dialog',
+        title: "Dialog",
+        link: "/pages/modal-overlays/dialog",
       },
       {
-        title: 'Window',
-        link: '/pages/modal-overlays/window',
+        title: "Window",
+        link: "/pages/modal-overlays/window",
       },
       {
-        title: 'Popover',
-        link: '/pages/modal-overlays/popover',
+        title: "Popover",
+        link: "/pages/modal-overlays/popover",
       },
       {
-        title: 'Toastr',
-        link: '/pages/modal-overlays/toastr',
+        title: "Toastr",
+        link: "/pages/modal-overlays/toastr",
       },
       {
-        title: 'Tooltip',
-        link: '/pages/modal-overlays/tooltip',
+        title: "Tooltip",
+        link: "/pages/modal-overlays/tooltip",
       },
     ],
   },
   {
-    title: 'Extra Components',
-    icon: 'message-circle-outline',
+    title: "Extra Components",
+    icon: "message-circle-outline",
     children: [
       {
-        title: 'Calendar',
-        link: '/pages/extra-components/calendar',
-      }
-    ],
-  },
-  {
-    title: 'Charts',
-    icon: 'pie-chart-outline',
-    children: [
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      }
-    ],
-  },
-  {
-    title: 'Tables & Data',
-    icon: 'grid-outline',
-    children: [
-      {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
-      },
-      {
-        title: 'Tree Grid',
-        link: '/pages/tables/tree-grid',
+        title: "Calendar",
+        link: "/pages/extra-components/calendar",
       },
     ],
   },
   {
-    title: 'Auth',
-    icon: 'lock-outline',
+    title: "Charts",
+    icon: "pie-chart-outline",
     children: [
       {
-        title: 'Login',
-        link: '/auth/login',
+        title: "Echarts",
+        link: "/pages/charts/echarts",
+      },
+    ],
+  },
+  {
+    title: "Tables & Data",
+    icon: "grid-outline",
+    children: [
+      {
+        title: "Smart Table",
+        link: "/pages/tables/smart-table",
       },
       {
-        title: 'Register',
-        link: '/auth/register',
+        title: "Tree Grid",
+        link: "/pages/tables/tree-grid",
+      },
+    ],
+  },
+  {
+    title: "Auth",
+    icon: "lock-outline",
+    children: [
+      {
+        title: "Login",
+        link: "/auth/login",
       },
       {
-        title: 'Request Password',
-        link: '/auth/request-password',
+        title: "Register",
+        link: "/auth/register",
       },
       {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
+        title: "Request Password",
+        link: "/auth/request-password",
+      },
+      {
+        title: "Reset Password",
+        link: "/auth/reset-password",
       },
     ],
   },
--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,5 +1,9 @@
-import {test} from '@playwright/test';
+import { test } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+   await page.getByText("Forms").click();
+   await page.getByText("Form Layouts").click();
 
 test.beforeEach(async ({page}) => {
     await page.goto('http://localhost:4200/');
@@ -55,32 +59,97 @@ test.only('User facing locators', async({page}) => {
 
 });
 
-test.describe.only('suite1', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Charts').click()
-    });
+test("Locator syntax rules", async ({ page }) => {
+  // by Tag name
+  await page.locator("input").first().click();
 
-    test('the first test', async ({page}) => {
-        await page.getByText('Form Layouts').click()
-    });
-    
-    test('navigate to datepicker page', async ({page}) => {
-        await page.getByText('Datepicker').click()
-    });
+  // by ID
+  page.locator("#inputEmail1");
+
+  // by Class value
+  page.locator(".shape-rectangle");
+
+  // by Attribute
+  page.locator('[placeholder="Email"]');
+
+  // by Class value (full)
+  page.locator(
+    '[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]'
+  );
+
+  // Combine different selectors
+  page.locator('input[placeholder="Email"][nbinput]');
+
+  // By XPath (not recommended)
+  page.locator('//*[@id="inputEmail1"]');
+
+  // By partial text match
+  page.locator(':text("Using")');
+
+  // By exact text math
+  page.locator(':text-is("Using the Grid")');
 });
 
-test.describe('suite1', () => {
-    test.beforeEach(async ({page}) => {
-        await page.getByText('Forms').click();
-    });
+test("User facing locators", async ({ page }) => {
+  await page.getByRole("textbox", { name: "Email" }).first().click(); // By Role
+  await page.getByRole("button", { name: "Sign in" }).first().click();
 
-    test('the first test1', async ({page}) => {
-        await page.getByText('Form Layouts').click();
-    });
-    
-    test('navigate to datepicker page1', async ({page}) => {
-        await page.getByText('Datepicker').click();
-    });
+  await page.getByLabel("Email").first().click(); // By Label
+
+  await page.getByPlaceholder("Jane Doe").click(); //By Placeholder
+
+  await page.getByText("Using the Grid").click(); //By Text
+
+  await page.getByTestId("SignIn").click(); // By data test id
+
+  await page.getByTitle("IoT Dashboard").click(); // By Title
+});
+
+test.describe("Chart tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts").first().click();
+    await page.getByText("Echarts").click();
+  });
+
+  test("the Pie test", async ({ page }) => {
+    await page.getByText("Pie").isVisible();
+  });
+});
+
+test.describe("Form tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+
+  test("the first test1", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("navigate to datepicker page1", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
+});
+
+test.only('Locating child elements', async ({page}) => {
+    await page.locator('nb-card nb-radio :text-is("Option 1")').click();// same as next line locators
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();//chaining
+
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click()
+
+    await page.locator('nb-card').nth(3).getByRole('button').click();// try to avoid to select by index
 })
 
+test('locating parent elements', async({page}) => {
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click();
+    await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', { name: "Email" }).click();
+
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox', {name: "Email"}).click();
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click();
+
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"})
+        .getByRole('textbox', { name: "Email" }).click();
+
+    await page.locator (':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: "Email" }).click();
+
+})
 
