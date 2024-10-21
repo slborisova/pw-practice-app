ca5e8bcf6cfecf5ae58fc89decec61ac83d80f17
slborisova <slborisova@gmail.com>
2024-10-21 13:33:59 -0400

    reusing locatirs

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,61 +1,61 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
    await page.getByText("Forms").click();
    await page.getByText("Form Layouts").click();
 
-test.beforeEach(async ({page}) => {
-    await page.goto('http://localhost:4200/');
+// test.beforeEach(async ({page}) => {
+//     await page.goto('http://localhost:4200/');
 
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click();
-});
+//     await page.getByText('Forms').click();
+//     await page.getByText('Form Layouts').click();
+// });
 
-test('Locator syntax rules', async ({page}) => {
-    // by Tag name
-    await page.locator('input').first().click();
+// test('Locator syntax rules', async ({page}) => {
+//     // by Tag name
+//     await page.locator('input').first().click();
 
-    // by ID
-    page.locator('#inputEmail1');
+//     // by ID
+//     page.locator('#inputEmail1');
 
-    // by Class value
-    page.locator('.shape-rectangle');
+//     // by Class value
+//     page.locator('.shape-rectangle');
 
-    // by Attribute
-    page.locator('[placeholder="Email"]');
+//     // by Attribute
+//     page.locator('[placeholder="Email"]');
 
-    // by Class value (full)
-    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
+//     // by Class value (full)
+//     page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
 
-    // Combine different selectors
-    page.locator('input[placeholder="Email"][nbinput]');
+//     // Combine different selectors
+//     page.locator('input[placeholder="Email"][nbinput]');
 
-    // By XPath (not recommended)
-    page.locator('//*[@id="inputEmail1"]');
+//     // By XPath (not recommended)
+//     page.locator('//*[@id="inputEmail1"]');
 
-    // By partial text match
-    page.locator(':text("Using")');
+//     // By partial text match
+//     page.locator(':text("Using")');
 
-    // By exact text math
-    page.locator(':text-is("Using the Grid")')
-})
+//     // By exact text math
+//     page.locator(':text-is("Using the Grid")')
+// })
 
-test.only('User facing locators', async({page}) => {
-    await page.getByRole('textbox', {name: "Email"}).first().click(); // By Role
-    await page.getByRole('button', {name: "Sign in"}).first().click();
+// test('User facing locators', async({page}) => {
+//     await page.getByRole('textbox', {name: "Email"}).first().click(); // By Role
+//     await page.getByRole('button', {name: "Sign in"}).first().click();
 
-    await page.getByLabel('Email').first().click(); // By Label
+//     await page.getByLabel('Email').first().click(); // By Label
 
-    await page.getByPlaceholder('Jane Doe').click(); //By Placeholder
+//     await page.getByPlaceholder('Jane Doe').click(); //By Placeholder
 
-    await page.getByText('Using the Grid').click(); //By Text
+//     await page.getByText('Using the Grid').click(); //By Text
 
-    await page.getByTestId('SignIn').click(); // By data test id
+//     await page.getByTestId('SignIn').click(); // By data test id
 
-    await page.getByTitle('IoT Dashboard').click(); // By Title
+//     await page.getByTitle('IoT Dashboard').click(); // By Title
 
-})
+// })
 
 });
 
@@ -130,7 +130,7 @@ test.describe("Form tests", () => {
   });
 });
 
-test.only('Locating child elements', async ({page}) => {
+test('Locating child elements', async ({page}) => {
     await page.locator('nb-card nb-radio :text-is("Option 1")').click();// same as next line locators
     await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();//chaining
 
@@ -150,6 +150,17 @@ test('locating parent elements', async({page}) => {
         .getByRole('textbox', { name: "Email" }).click();
 
     await page.locator (':text-is("Using the Grid")').locator('..').getByRole('textbox', { name: "Email" }).click();
+})
 
+test('Reusing the locators', async ({page}) => {
+    const basicForm = page.locator('nb-card').filter({hasText: "Basic form"});
+    const emailField = basicForm.getByRole('textbox', {name: "Email"})
+
+    await emailField.fill('test@test.com');
+    await basicForm.getByRole('textbox', {name: "Password"}).fill('Welcome123');
+    await basicForm.locator('nb-checkbox').click();
+    await basicForm.getByRole('button').click();
+
+    await expect(emailField).toHaveValue('test@test.com');
 })
 
