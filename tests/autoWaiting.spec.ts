e39d2d9d8d236d77bd04a44b64462b00ff41bd6b
slborisova <slborisova@gmail.com>
2024-10-26 10:38:17 -0400

    auto watis tests added

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("http://uitestingplayground.com/ajax");
+    await page.getByText("Button Triggering AJAX Request").click();
+});
+
+test('Auto waiting', async({page}) => {
+    const successButton = page.locator('.bg-success');
+
+    // await successButton.click();
+
+    // const text = await successButton.textContent();
+    // await successButton.waitFor({state: "attached"})
+    // const text = await successButton.allTextContents();
+
+    // expect(text).toContain("Data loaded with AJAX get request."); 
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000});
+})
+
+test('Alternative waits', async({page}) => {
+    const successButton = page.locator('.bg-success');
+
+    //___wait for element
+    // await page.waitForSelector('.bg-success');
+
+    // ___wait for particular response
+    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata');
+
+    // wait for network calls to be completed ('NOT RECOMMENDED')
+    await page.waitForLoadState('networkidle');
+
+    
+    const text = await successButton.allTextContents();
+    expect(text).toContain("Data loaded with AJAX get request."); 
+})
+
+
+
--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,59 +4,6 @@ test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
    await page.getByText("Forms").click();
    await page.getByText("Form Layouts").click();
-
-// test.beforeEach(async ({page}) => {
-//     await page.goto('http://localhost:4200/');
-
-//     await page.getByText('Forms').click();
-//     await page.getByText('Form Layouts').click();
-// });
-
-// test('Locator syntax rules', async ({page}) => {
-//     // by Tag name
-//     await page.locator('input').first().click();
-
-//     // by ID
-//     page.locator('#inputEmail1');
-
-//     // by Class value
-//     page.locator('.shape-rectangle');
-
-//     // by Attribute
-//     page.locator('[placeholder="Email"]');
-
-//     // by Class value (full)
-//     page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]');
-
-//     // Combine different selectors
-//     page.locator('input[placeholder="Email"][nbinput]');
-
-//     // By XPath (not recommended)
-//     page.locator('//*[@id="inputEmail1"]');
-
-//     // By partial text match
-//     page.locator(':text("Using")');
-
-//     // By exact text math
-//     page.locator(':text-is("Using the Grid")')
-// })
-
-// test('User facing locators', async({page}) => {
-//     await page.getByRole('textbox', {name: "Email"}).first().click(); // By Role
-//     await page.getByRole('button', {name: "Sign in"}).first().click();
-
-//     await page.getByLabel('Email').first().click(); // By Label
-
-//     await page.getByPlaceholder('Jane Doe').click(); //By Placeholder
-
-//     await page.getByText('Using the Grid').click(); //By Text
-
-//     await page.getByTestId('SignIn').click(); // By data test id
-
-//     await page.getByTitle('IoT Dashboard').click(); // By Title
-
-// })
-
 });
 
 test("Locator syntax rules", async ({ page }) => {
@@ -183,6 +130,22 @@ test('Extracting values', async({page}) => {
     // any attributes
     const placeholderValue = await emailField.getAttribute('placeholder');
     expect (placeholderValue).toEqual('Email')
+});
 
-})
+test('Assertions', async ({page}) => {
+    const basicFormButton = page.locator('nb-card').filter({hasText: "Basic form"}).locator('button');
+    // General assertions
+    const value = 5;
+    expect(value).toEqual(5);
+
+    const text = await basicFormButton.textContent();
+    expect(text).toEqual("Submit");
+
+    // Locator assertions
+    await expect(basicFormButton).toHaveText('Submit');
+    
+    // Soft assertions
+    await expect.soft(basicFormButton).toHaveText("Submit");// not good practice
+    await basicFormButton.click();
+});
 
