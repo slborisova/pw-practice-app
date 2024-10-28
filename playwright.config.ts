c3bbd5de5657d9ee11bce847511fa69e5b0832af
slborisova <slborisova@gmail.com>
2024-10-27 22:49:33 -0400

    Timeouts test added

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,12 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // timeout: 10000,
+  timeout: 10000,
+  globalTimeout: 60000,
+
+  expect:{
+    timeout: 2000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,6 +36,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // actionTimeout: 5000,
+    navigationTimeout: 5000,
   },
 
   /* Configure projects for major browsers */
--- tests/autoWaiting.spec.ts
+++ tests/autoWaiting.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ page }, testInfo) => {
     await page.goto("http://uitestingplayground.com/ajax");
     await page.getByText("Button Triggering AJAX Request").click();
+    testInfo.setTimeout(testInfo.timeout + 2000)
 });
 
 test('Auto waiting', async({page}) => {
@@ -30,10 +31,16 @@ test('Alternative waits', async({page}) => {
 
     // wait for network calls to be completed ('NOT RECOMMENDED')
     await page.waitForLoadState('networkidle');
-
     
     const text = await successButton.allTextContents();
     expect(text).toContain("Data loaded with AJAX get request."); 
+});
+
+test('Timeouts', async({page}) => {
+    // test.setTimeout(10000);
+    test.slow()
+    const successButton = page.locator('.bg-success');
+    await successButton.click();
 })
 
 
