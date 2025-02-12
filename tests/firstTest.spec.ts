59e7ae67fbb268047a0e228ae33bdf8d6d652459
slborisova <slborisova@gmail.com>
2025-02-11 22:29:23 -0500

    commit new

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
-   await page.getByText("Forms").click();
-   await page.getByText("Form Layouts").click();
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
 });
 
 test("Locator syntax rules", async ({ page }) => {
