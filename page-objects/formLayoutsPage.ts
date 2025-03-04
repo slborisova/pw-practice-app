670986fc28ea261b957bb42f731927bbc00e8853
slborisova <slborisova@gmail.com>
2025-03-04 12:41:36 -0500

    helperBase created

--- page-objects/formLayoutsPage.ts
+++ page-objects/formLayoutsPage.ts
@@ -1,10 +1,10 @@
 import { Page } from "@playwright/test";
+import { HelperBase } from "./helperBase";
 
-export class FormLayoutsPage {
+export class FormLayoutsPage extends HelperBase{
 
-    private readonly page: Page;
     constructor(page: Page){
-        this.page = page;
+        super(page);
     }
 
 
