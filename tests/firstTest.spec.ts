a23becde7455c2f3b43340a12a2c9d953538d235
slborisova <slborisova@gmail.com>
2025-11-17 19:35:10 -0500

    updated data

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("http://localhost:4200/");
 
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
   await page.getByText("Forms").click();
   await page.getByText("Form Layouts").click();
-
 });
 
 test("Locator syntax rules", async ({ page }) => {
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../page-objects/pageManager';
+import { faker } from '@faker-js/faker';
 
 
 test.beforeEach(async({page}) => {
-    await page.goto('http://localhost:4200/')
+    await page.goto('/')
 });
 
-test('navigate to form page', async({page}) =>{
+test('navigate to form page @smoke @regression', async({page}) =>{
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage();
     await pm.navigateTo().datepickerPage();
@@ -15,11 +16,18 @@ test('navigate to form page', async({page}) =>{
     await pm.navigateTo().tooltipPage();
 });
 
-test('parametrized methods', async ({page}) => {
+test('parametrized methods @smoke', async ({page}) => {
     const pm = new PageManager(page)
+    const randomFullName = faker.person.fullName();
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`;
+
     await pm.navigateTo().formLayoutsPage();
-    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2' );
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false);
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption(process.env.USERNAME, process.env.PASSWORD, 'Option 2' );
+    // await page.screenshot({path: 'screenshots/formsLayoutsPage.png'});
+    // const buffer = await page.screenshot();
+    // console.log(buffer.toString('base64'));
+    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false);
+    // await page.locator("nb-card", {hasText: "Inline form"}).screenshot({path: 'screenshots/inlineForm.png'});
     await pm.navigateTo().datepickerPage();
     await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10);
     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10);
